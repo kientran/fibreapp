8abbf1ca94e6d87122148b2a195f6246078aaae3
Kien Tran <kien@kientran.com>
2011-05-28 15:19:39 -0500

    Add parsing code

--- fburl.rb
+++ fburl.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'ri_cal'
+require 'date'
+require 'open-uri'
 
 get '/' do
   'hello world!'
@@ -9,4 +12,11 @@ get '/webcal://www.facebook.com/ical/b.php' do
   key = params['key']
   
   "UID #{uid}  KEY #{key}"
+  
+  calendars = RiCal.parse(open("http://www.facebook.com/ical/b.php?uid=#{uid}&key=#{key}"))
+
+  calendars.first.events.each do |event|
+    event.dtstart = Date.parse(event.dtstart.to_s) << 12
+  end
+  calendars.first.export
 end
